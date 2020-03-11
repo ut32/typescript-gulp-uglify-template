d0bcbe2d83ce4a3c3563d881dfbf3003d5a40737
the7 <one_byte@gmail.com>
2020-03-11 13:24:14 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var tsify = require("tsify");
+var watchify = require("watchify");
+var gutil = require("gulp-util");
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+
+var paths = {
+    pages: ['src/*.html']
+};
+
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
+
+gulp.task("copy-html", function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest("dist"));
+});
+
+function bundle() {
+    return watchedBrowserify
+        .plugin(tsify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest("dist"));
+}
+
+gulp.task("default",gulp.series("copy-html", bundle));
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
+
